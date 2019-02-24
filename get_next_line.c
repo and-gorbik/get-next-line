5129d002b2b5593b7244ee9bf8b3c1ab5b8fcebe
and-gorbik <and.gorbik@gmail.com>
2019-02-24 14:05:58 +0300

    update solution (new one with arrays against the one with lists)

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sjacelyn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/18 16:41:20 by sjacelyn          #+#    #+#             */
-/*   Updated: 2018/12/21 15:49:25 by sjacelyn         ###   ########.fr       */
+/*   Updated: 2018/12/23 21:35:58 by sjacelyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,92 +33,31 @@ static int	read_line_from_buffer(char **buf, char **line, char delim)
 		status = 0;
 		sub = *buf + ft_strlen(*buf);
 	}
-	if (!(chunk = ft_strnew(sub - *buf)))
-		return (-1);
+	CHECK_ERROR((chunk = ft_strnew(sub - *buf)));
 	prev = *line;
 	if (!(*line = ft_strjoin(prev, ft_strncpy(chunk, *buf, sub - *buf))))
 		status = -1;
 	else
-		ft_strcpy(*buf, sub + status);
-	free(chunk);
-	free(prev);
+		ft_strcpy(*buf, status ? sub + 1 : sub);
+	ft_memdel((void **)&chunk);
+	ft_memdel((void **)&prev);
 	return (status);
 }
 
 /*
-** this function create a new list item and returns a buffer.
-** lst and fd are always valid.
+** This function returns a buffer by fd
 */
 
-static char	*create_buffer(t_list **lst, int fd)
-{
-	t_buf	cur;
-	t_list	*item;
-
-	cur.fd = fd;
-	if (!(cur.buf = ft_strnew(BUFF_SIZE)))
-		return (NULL);
-	if (!(item = ft_lstnew(&cur, sizeof(t_buf))))
-	{
-		ft_memdel((void **)&cur.buf);
-		return (NULL);
-	}
-	ft_lstadd(lst, item);
-	return (cur.buf);
-}
-
-/*
-** if list item doesn't exist with such fd,
-** this function create a new list item and returns a buffer,
-** otherwise it returns an existing buffer.
-*/
-
-static char	*get_buffer(t_list **lst, int fd)
+static char	*get_buffer(char **bufs, int fd)
 {
 	char	b[1];
-	t_buf	*cur;
-	t_list	*item;
 
-	if (!lst || fd < 0 || read(fd, b, 0) < 0)
+	if (fd < 0 || fd == 1 || fd == 2 || fd >= FD_SIZE
+		|| read(fd, b, 0) < 0 || !bufs)
 		return (NULL);
-	item = *lst;
-	while (item)
-	{
-		cur = item->content;
-		if (cur->fd == fd)
-			return (cur->buf);
-		item = item->next;
-	}
-	return (create_buffer(lst, fd));
-}
-
-/*
-** this function removes an item with fd from the lst,
-** freeing the item's buffer
-*/
-
-static void	remove_buffer(t_list **lst, int fd)
-{
-	t_list	*prev;
-	t_list	*cur;
-	t_buf	*data;
-
-	cur = *lst;
-	prev = NULL;
-	while (cur)
-	{
-		data = cur->content;
-		if (data->fd == fd)
-			break ;
-		cur = cur->next;
-		prev = cur;
-	}
-	if (prev)
-		prev->next = (cur ? cur->next : NULL);
-	if (cur == *lst)
-		*lst = NULL;
-	ft_memdel((void **)&(data->buf));
-	ft_memdel((void **)&(cur));
+	if (!bufs[fd])
+		bufs[fd] = ft_strnew(BUFF_SIZE);
+	return (bufs[fd]);
 }
 
 /*
@@ -132,19 +71,18 @@ static void	remove_buffer(t_list **lst, int fd)
 
 int			get_next_line(const int fd, char **line)
 {
-	static t_list	*bufs;
+	static char		*bufs[FD_SIZE];
 	char			*buf;
 	int				status;
 
-	if (!line || !(buf = get_buffer(&bufs, fd)))
-		return (-1);
-	if (!(*line = ft_strnew(0)))
-		return (-1);
+	CHECK_ERROR(line && BUFF_SIZE > 0 && FD_SIZE > 0);
+	CHECK_ERROR((buf = get_buffer(bufs, fd)));
+	CHECK_ERROR((*line = ft_strnew(0)));
 	if (!buf[0])
 		buf[read(fd, buf, BUFF_SIZE)] = '\0';
 	if (!buf[0])
 	{
-		remove_buffer(&bufs, fd);
+		ft_memdel((void **)&bufs[fd]);
 		return (0);
 	}
 	while (buf[0])
--- get_next_line.h
+++ get_next_line.h
@@ -6,22 +6,18 @@
 /*   By: sjacelyn <sjacelyn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 17:13:28 by sjacelyn          #+#    #+#             */
-/*   Updated: 2018/12/21 15:40:39 by sjacelyn         ###   ########.fr       */
+/*   Updated: 2018/12/23 21:23:01 by sjacelyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# define BUFF_SIZE 21
+# define FD_SIZE 2048
+# define CHECK_ERROR(x) if (!x) return (-1);
+
 # include "libft/libft.h"
-
-# define BUFF_SIZE (1 << 8)
-
-typedef struct	s_buf
-{
-	int			fd;
-	char		*buf;
-}				t_buf;
 
 int				get_next_line(const int fd, char **line);
 
